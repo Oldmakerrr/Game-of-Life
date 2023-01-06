b55fbff3d50b2400398fabdc1249b093d80c2c0a
Vladimir Berezin <92182846+Oldmakerrr@users.noreply.github.com>
2023-01-06 11:44:48 +0400

    Initial Commit

---
+++ GameOfLife/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameOfLife
+//
+//  Created by Vladimir Berezin on 06.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
